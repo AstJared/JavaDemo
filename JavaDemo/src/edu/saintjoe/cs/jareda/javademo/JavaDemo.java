ca952dbfd87074780ae648ab29d18134bf0cc3be
jareda <jareda@localhost>
2016-02-08 16:29:06 -0500

    Initial Commit

---
+++ JavaDemo/src/edu/saintjoe/cs/jareda/javademo/JavaDemo.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.jareda.javademo;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Please stop making this app");
+		// TODO Auto-generated method stub
+
+	}
+
+}
